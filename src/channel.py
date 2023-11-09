15f7616b3e487af8d68e2515914c274611d5bbfc
Andrew_Polik <polik.24@mail.ru>
2023-11-09 13:35:39 +0300

    Прогон по pythoncheker

--- src/channel.py
+++ src/channel.py
@@ -2,18 +2,25 @@ import os
 from googleapiclient.discovery import build
 
 api_key: str = os.getenv('API_KEY_YOUTUBE')
-youtube = build('youtube', 'v3', developerKey=api_key)
+
+youtube = build('youtube', 'v3', developerKey = api_key)
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс для ютуб - канала"""
+
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """
+        Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API.
+        """
         self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel_id = self.channel_id
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        channel = (youtube.channels()
+                   .list(id = channel_id, part = 'snippet,statistics').execute())
         print(channel)
