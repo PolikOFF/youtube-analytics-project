b12e517072210cfb7018d62865d991be9ce270f6
Andrew_Polik <polik.24@mail.ru>
2023-11-09 11:11:42 +0300

    Initial. Аdded function: print_info.

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import os
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('API_KEY_YOUTUBE')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +10,10 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_id = self.channel_id
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        print(channel)
