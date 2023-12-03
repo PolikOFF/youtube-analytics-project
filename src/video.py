ee36264d7a075114f0d51e108472090a9803c307
Andrew_Polik <polik.24@mail.ru>
2023-12-04 01:24:29 +0300

    Добавлено исключение, если пользователь передаст некорректный id канала

--- src/video.py
+++ src/video.py
@@ -10,15 +10,21 @@ class Video:
 
     def __init__(self, video_id):
         self.video_id = video_id
-        request = self.youtube.videos().list(part="snippet,contentDetails,statistics",
-                                             id=video_id).execute()
-        self.name = request['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch?v=' + self.video_id
-        self.watch_count = request['items'][0]['statistics']['viewCount']
-        self.like_count = request['items'][0]['statistics']['likeCount']
+        try:
+            request = self.youtube.videos().list(part="snippet,contentDetails,statistics",
+                                                 id=video_id).execute()
+            self.title = request['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + self.video_id
+            self.watch_count = request['items'][0]['statistics']['viewCount']
+            self.like_count = request['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.watch_count = None
+            self.like_count = None
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class PLVideo(Video):
@@ -27,4 +33,4 @@ class PLVideo(Video):
         self.playlist_id = playlist_id
 
     def __str__(self):
-        return self.name
+        return self.title
